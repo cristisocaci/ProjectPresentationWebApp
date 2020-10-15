043bb861b493d204697f1bc3a2ddbd9ed5477911
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-10-15 10:46:20 +0300

    Tried to create db

---
+++ PresentationApp/WebApplication1/DataModel/Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DataModel
+{
+    public class Info
+    {
+        public int InfoId;
+        public string Type;
+        public string Content;
+
+        public int ProjectId;
+        public Project Project;
+    }
+}
---
+++ PresentationApp/WebApplication1/DataModel/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DataModel
+{
+    public class Project
+    {
+        public int ProjectId;
+        public string Titlel;
+
+        public ICollection<Info> Infos;
+    }
+}
