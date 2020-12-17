d00728ef28e51230ce49c90d9273d3d137a66ca1
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-17 23:13:01 +0200

    i dont know what im doing

--- PresentationApp/WebApi/Program.cs
+++ PresentationApp/WebApi/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WebApi.DataModel;
+using ExtensionMethods;
 
 namespace WebApi
 {
@@ -17,7 +18,7 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().MigrateDatabase<MyProjectsContext>().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -31,4 +32,29 @@ namespace WebApi
 
     }
 
+}
+
+namespace ExtensionMethods
+{
+    public static class IWebHostExtensions
+    {
+        public static IWebHost MigrateDatabase<T>(this IWebHost webHost) where T : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetRequiredService<T>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<DbContext>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            return webHost;
+        }
+    }
 }
--- PresentationApp/WebApi/Startup.cs
+++ PresentationApp/WebApi/Startup.cs
@@ -51,8 +51,7 @@ namespace WebApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                     .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
             services.AddEntityFrameworkSqlServer();
-            services.AddDbContextPool<MyProjectsContext>(options => options.UseNpgsql(connectionString,
-                                                            x => x.MigrationsAssembly("WebApi")));
+            services.AddDbContextPool<MyProjectsContext>(options => options.UseNpgsql(connectionString));
             services.AddScoped<IMyProjectsRepository, MyProjectsRepository>();
             services.AddTransient<ITokenService, TokenService>();
             services.AddCors(opt => opt.AddPolicy("MyPolicy", builder =>
