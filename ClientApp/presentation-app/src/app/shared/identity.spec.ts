c5e758eac0119a190fe893f514f266aace9b8614
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-17 17:32:21 +0200

    moved the client app to another repo

--- ClientApp/presentation-app/src/app/shared/identity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Identity } from './identity';
-
-describe('Identity', () => {
-  it('should create an instance', () => {
-    expect(new Identity()).toBeTruthy();
-  });
-});
--- ClientApp/presentation-app/src/app/shared/info.ts
+++
@@ -1,8 +0,0 @@
-export class Info {
-    infoId: string;
-    type: string;
-    content: string;
-    position: number;
-    projectId: number;
-    additionalData: string;
-}
