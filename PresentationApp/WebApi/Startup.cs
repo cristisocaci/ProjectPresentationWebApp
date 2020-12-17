40fdc57751a7e29b9f050e12dbee3db9a6375740
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-17 23:02:40 +0200

    trying

--- PresentationApp/WebApi/Startup.cs
+++ PresentationApp/WebApi/Startup.cs
@@ -51,7 +51,8 @@ namespace WebApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                     .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
             services.AddEntityFrameworkSqlServer();
-            services.AddDbContextPool<MyProjectsContext>(options => options.UseNpgsql(connectionString));
+            services.AddDbContextPool<MyProjectsContext>(options => options.UseNpgsql(connectionString,
+                                                            x => x.MigrationsAssembly("WebApi")));
             services.AddScoped<IMyProjectsRepository, MyProjectsRepository>();
             services.AddTransient<ITokenService, TokenService>();
             services.AddCors(opt => opt.AddPolicy("MyPolicy", builder =>
