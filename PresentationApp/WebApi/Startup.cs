3197f59e79d3499acc80428cccb8888066458f25
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-17 22:47:52 +0200

    ...

--- PresentationApp/WebApi/Startup.cs
+++ PresentationApp/WebApi/Startup.cs
@@ -35,23 +35,18 @@ namespace WebApi
 
             string connectionString = null;
             string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
-            if (string.IsNullOrEmpty(envVar))
-            {
-                connectionString = Configuration["Connectionstrings:database"];
-            }
-            else
-            {
-                //parse database URL. Format is postgres://<username>:<password>@<host>/<dbname>
-                var uri = new Uri(envVar);
-                var username = uri.UserInfo.Split(':')[0];
-                var password = uri.UserInfo.Split(':')[1];
-                connectionString =
-                "; Database=" + uri.AbsolutePath.Substring(1) +
-                "; Username=" + username +
-                "; Password=" + password +
-                "; Port=" + uri.Port +
-                "; SSL Mode=Require; Trust Server Certificate=true;";
-            }
+
+            //parse database URL. Format is postgres://<username>:<password>@<host>/<dbname>
+            var uri = new Uri(envVar);
+            var username = uri.UserInfo.Split(':')[0];
+            var password = uri.UserInfo.Split(':')[1];
+            connectionString =
+            "; Database=" + uri.AbsolutePath.Substring(1) +
+            "; Username=" + username +
+            "; Password=" + password +
+            "; Port=" + uri.Port +
+            "; SSL Mode=Require; Trust Server Certificate=true;";
+            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                     .AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
