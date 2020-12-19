fcbf39f9db975877d80b16b23b21d025fbc65453
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-19 11:35:02 +0200

    trying to migrate the database on heroku

--- PresentationApp/WebApi/Program.cs
+++ PresentationApp/WebApi/Program.cs
@@ -17,7 +17,10 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .Build()
+                .MigrateDatabase<MyProjectsContext>()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
@@ -30,6 +33,28 @@ namespace WebApi
                 .UseStartup<Startup>()
                 .UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
 
+    }
+
+    public static class IWebHostExtensions
+    {
+        public static IWebHost MigrateDatabase<T>(this IWebHost webHost) where T : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetRequiredService<T>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+            return webHost;
+        }
     }
 
 }
