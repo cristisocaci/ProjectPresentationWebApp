e3928cb8975f31077e00e10afac614fd32041978
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2020-12-27 20:30:04 +0200

    deleted migrations

--- PresentationApp/WebApi/Program.cs
+++ PresentationApp/WebApi/Program.cs
@@ -19,7 +19,6 @@ namespace WebApi
         {
             CreateWebHostBuilder(args)
                 .Build()
-                .MigrateDatabase<MyProjectsContext>()
                 .Run();
         }
 
@@ -31,28 +30,6 @@ namespace WebApi
                     logging.AddConsole();
                 })
                 .UseStartup<Startup>();
-    }
-
-    public static class IWebHostExtensions
-    {
-        public static IWebHost MigrateDatabase<T>(this IWebHost webHost) where T : DbContext
-        {
-            using (var scope = webHost.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var db = services.GetRequiredService<T>();
-                    db.Database.Migrate();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while migrating the database.");
-                }
-            }
-            return webHost;
-        }
     }
 
 }
